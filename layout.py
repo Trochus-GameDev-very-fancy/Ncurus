f9e057076dd6292b91dae6f7387b2cd8ba51ac45
Tim-ats-d <tim.arnouts@protonmail.com>
2021-05-02 16:40:50 +0200

    Develop Layout class.

--- layout.py
+++ layout.py
@@ -30,12 +30,16 @@ class Pad:
 class Gallery(Pad):
     def __init__(self, win: CursesWin):
         self.win = win
+        # self.win.bkgdset("t")
+
+    def update(self) -> ConsoleEffect:
+        ...
 
 
 class Dialogs(Pad):
     def __init__(self, win: CursesWin):
         self.win = win
-        # self.win.bkgdset("o")
+        # self.win.bkgdset("b")
 
         self.textbox = DialogBox(0, 0, 20, 6, "TextBox")
 
@@ -47,36 +51,62 @@ class Layout:
     """Encapsulate a layout to manage a set of windows more
     efficiently.
     """
+    side_borders_total_width = 2
+
     def __init__(self, win: CursesWin):
-        side_borders_total_width = 2
         max_y, max_x = win.getmaxyx()
 
-        self.stdscr = win  # Original win.
-        self.win: CursesWin = win.subwin(max_y - side_borders_total_width,
-                                         max_x - side_borders_total_width,
-                                         1, 1)
+        self.stdscr = win  # Catch original win.
+        self.win = win.subwin(max_y - Layout.side_borders_total_width,
+                              max_x - Layout.side_borders_total_width,
+                              1, 1)
 
-        self.pads: list = []
+        self.pads = []
 
     def add_pad(self, *pads: Pad) -> NoReturn:
         """Add given pads to list of pads managed by the layout."""
         for pad in pads:
             self.pads.append(pad)
 
+    def create_top_win(self) -> CursesWin:
+        return self.win.subwin(self.half_y,
+                               self.max_x,
+                               1, 1)
+
     def create_bottom_win(self) -> CursesWin:
-        max_y, max_x = self.win.getmaxyx()
-        half_y = max_y // 2
+        return self.win.subwin(self.half_y + 2,
+                               1)
 
-        return self.win.subwin(half_y, 1)
+    @property
+    def max_x(self) -> int:
+        """Return number of column of windows."""
+        return self.win.getmaxyx()[1]
 
-    def update(self) -> ConsoleEffect:
+    @property
+    def max_y(self) -> int:
+        """Return number of line of windows."""
+        return self.win.getmaxyx()[0]
+
+    @property
+    def half_y(self) -> int:
+        """Return half of the total height window."""
+        return self.max_y // 2
+
+    def draw_borders(self) -> ConsoleEffect:
+        """Draw layout borders."""
         self.stdscr.clear()
         self.stdscr.box()
+
+        # Separation line.
+        self.stdscr.hline(self.half_y + 1, 1,
+                          "-", self.max_x - 1)
+
         self.stdscr.refresh()
 
     def routine(self) -> ConsoleEffect:
+        """Clear, call update method and refresh all pad."""
         if self.stdscr.is_wintouched():
-            self.update()
+            self.draw_borders()
 
         for pad in self.pads:
             pad.clear()
@@ -88,7 +118,7 @@ def main(win):
     curses.curs_set(0)
 
     layout = Layout(win)
-    gallery = Gallery(layout.win)
+    gallery = Gallery(layout.create_top_win())
     dialogs = Dialogs(layout.create_bottom_win())
 
     layout.add_pad(gallery, dialogs)
